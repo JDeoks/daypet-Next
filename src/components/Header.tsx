647a3e89ccb50431b93d394091866bbcecf8f144
JDeoks <duginee@naver.com>
2024-08-15 02:42:17 +0900

    Feat: Header 패딩 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function MainSection() {
     return (
         <>
             <div className="fixed left-0 top-0 right-0 shadow h-16 flex justify-center bg-white">
-                <div className="w-900 flex ">
+                <div className="w-900 flex pl-3">
                     <Image src={navBarAppIcon} alt="" width={40} height={40} />
                 </div>
             </div>
